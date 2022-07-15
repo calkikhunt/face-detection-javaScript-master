b452f4b18e48622e46166c4d472ff39dc6ffa80a
calkikhunt <calkikhunt123@gmail.com>
2022-07-15 17:33:01 +0530

    [ADD] project files

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
+
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/')
+@cross_origin()
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
